ee0f8697d7b4234090d5a94044c68296056cc011
Shaan <shanali.ozhorik@Gmail.com>
2024-08-29 05:10:13 +0500

    commited

---
+++ data_structures/sets.js
@@ -0,0 +1,4 @@
+//                     ======== SETS ========
+
+/* A set is also like an array. Unlike an array, sets does not contain duplicate values and are not arranged in a specific order (not sorted). */
+
