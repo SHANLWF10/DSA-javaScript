af9c6d753fd825ead0afce0de63625494ea56145
Shaan <shanali.ozhorik@Gmail.com>
2024-08-29 07:35:20 +0500

    commited queue and sets

---
+++ data_structures/binary-search-tree.js
@@ -0,0 +1,3 @@
+//        ========  BINARY SEARCH TREE ========
+
+/* A tree data structure is a way to hold data, which visualized looks like an actual tree, but upside down :D. The data points in the tree are called Nodes. The top one is called the root node. A node with branches leading to other nodes are called parent nodes. Leaf nodes are the nodes */
