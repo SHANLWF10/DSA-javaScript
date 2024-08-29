ffc0b239440113d2aa355e34cadabede2b56949a
Shaan <shanali.ozhorik@Gmail.com>
2024-08-29 07:20:57 +0500

    commited queue and sets

---
+++ data_structures/queue.js
@@ -0,0 +1,44 @@
+//      ======== QUEUE =======
+/* 
+The queue is similar to a stack, but with one major difference, that a queue follows a "first in first out"(FIFO) principle. Which means the item added first will be removed first.
+*/
+
+function Queue() {
+  collection = [];
+  // this shows whats in our queue
+  this.print = function () {
+    console.log(collection);
+  };
+
+  // This adds the first element to the queue
+  this.enqueue = function (element) {
+    collection.push(element);
+  };
+
+  // this removes the first element of the queue
+  this.dequeue = function () {
+    return collection.shift();
+  };
+
+  // this returns the first element of the queue without removing it
+  this.front = function () {
+    return collection[0];
+  };
+
+  // this returns the size of our queue
+  this.size = function () {
+    return collection.length;
+  };
+
+  // this check if our queue is empty or not
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
+}
+
+let q = new Queue();
+
+q.enqueue("a");
+q.enqueue("b");
+q.enqueue("c");
+q.print();
